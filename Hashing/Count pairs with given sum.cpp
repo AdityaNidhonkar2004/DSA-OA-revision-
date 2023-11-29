52a86e8e94b59fae93e3a828bc15d546b2eadfa9
Aditya Nidhonkar <adityanidhonkar2004@gmail.com>
2023-11-29 11:06:05 +0530

    done

---
+++ Hashing/Count pairs with given sum.cpp
@@ -0,0 +1,22 @@
+// Given an array of N integers, and an integer K, the task is to find the number of pairs of integers in the array whose sum is equal to K.
+
+// Examples:
+
+// Input: arr[] = {1, 5, 7, -1}, K = 6
+// Output:  2
+// Explanation: Pairs with sum 6 are (1, 5) and (7, -1).
+
+// Input: arr[] = {1, 5, 7, -1, 5}, K = 6
+// Output:  3
+// Explanation: Pairs with sum 6 are (1, 5), (7, -1) & (1, 5).
+
+// Input: arr[] = {1, 1, 1, 1}, K = 2
+// Output:  6
+// Explanation: Pairs with sum 2 are (1, 1), (1, 1), (1, 1), (1, 1), (1, 1).
+
+// Input: arr[] = {10, 12, 10, 15, -1, 7, 6, 5, 4, 2, 1, 1, 1}, K = 11
+// Output:  9
+// Explanation: Pairs with sum 11 are (10, 1), (10, 1), (10, 1), (12, -1), (10, 1), (10, 1), (10, 1), (7, 4), (6, 5).
+
+// Approach:
+// -same as two sum problem.
